c85130ebb143d36b3b90f6e7455f15da9a0f788b
Oribiel <153157241+Oribiel@users.noreply.github.com>
2025-02-21 12:31:42 -0600

    Conexion a base de datos
    c

---
+++ src/main/java/Persistencia/ConexionBD.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author oribi
+ */
+public class ConexionBD implements IConexionBD {
+
+    //BASE DE DATOS CON EL QUE CONECTAMOS A MYSQL
+    private final String USUARIO = "root";
+    private final String CONTRASEÑA = "1234";
+    //NECESARIOS PARA LA CONEXION
+    private final String SERVIDOR = "127.0.0.1";//localhost
+    private final String BASE_DE_DATOS = "LaboratoriosBD";
+    private final String URL = "jdbc:mysql://" + SERVIDOR + "/" + BASE_DE_DATOS;
+
+    @Override
+    public Connection crearConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+    }
+
+}
