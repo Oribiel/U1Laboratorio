4bf32609efbc4afffe9974ca0f90edd91b46e27a
Oribiel <153157241+Oribiel@users.noreply.github.com>
2025-02-21 12:10:16 -0600

    primer commit

---
+++ src/main/java/Main/U1Laboratorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author oribi
+ */
+public class U1Laboratorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
